46f155ec7533546e03d88eded39d27711b64c778
PierreDAVOINE <davoine.pierre@hotmail.fr>
2023-09-10 16:58:27 +0200

    Add images assets and start Header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
-
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 </script>
 
-<template></template>
+<template>
+    <Header />
+    <Footer />
+</template>
 
 <style scoped lang="scss"></style>
