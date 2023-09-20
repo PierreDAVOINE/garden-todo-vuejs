7b57b17c8003654fc19102be7041e310685bc3ef
PierreDAVOINE <davoine.pierre@hotmail.fr>
2023-09-20 12:43:24 +0200

    Add plant list page. Add plantCard component.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,9 +4,6 @@
 </script>
 
 <template>
-    <!-- <router-view :allPokemons="pokemons" :pokemons="pokemonFiltered" :isSearchOpen="isSearchOpen"
-        @update:isSearchOpen="updateIsSearchOpen" @update:inputSearch="updateInputSearch" :idPokemonAnchor="idPokemonAnchor"
-        @update:idPokemonAnchor="updateIdPokemonAnchor"></router-view> -->
     <router-view></router-view>
 </template>
 
